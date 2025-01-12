c54a2768b1d6c986f5b58a062ec497d2b1c18f06
Derk Norton <derk.norton@gmail.com>
2025-01-12 09:36:03 -0700

    Added a go.mod file.

---
+++ v2/Module.go
@@ -0,0 +1,16 @@
+/*
+................................................................................
+.    Copyright (c) 2009-2025 Crater Dog Technologies.  All Rights Reserved.    .
+................................................................................
+.  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.               .
+.                                                                              .
+.  This code is free software; you can redistribute it and/or modify it under  .
+.  the terms of The MIT License (MIT), as published by the Open Source         .
+.  Initiative. (See https://opensource.org/license/MIT)                        .
+................................................................................
+*/
+
+/*
+Package "module" defines the global functions provided by this module.
+*/
+package module
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/craterdog/go-simple-processor/v2
+
+go 1.23
